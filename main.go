4ac2b106868a3134b7b839b5f5234e7dc64803f8
Alex Mitchell <shamsup@users.noreply.github.com>
2024-12-30 14:11:50 -0800

    add commands processing

--- main.go
+++ main.go
@@ -7,7 +7,24 @@ import (
 	"strings"
 )
 
+var commands = map[string]Command{}
+
+func registerCommand(cmd Command) {
+	commands[cmd.Name] = cmd
+}
+
 func main() {
+	registerCommand(Command{
+		Name:        "help",
+		Description: "Displays a help message",
+		Handler:     commandHelp,
+	})
+	registerCommand(Command{
+		Name:        "exit",
+		Description: "Exit the Pokedex",
+		Handler:     commandExit,
+	})
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
@@ -15,7 +32,13 @@ func main() {
 			words := cleanInput(scanner.Text())
 			if len(words) > 0 {
 				command := words[0]
-				fmt.Printf("Your command was: %s\n", command)
+				if cmd, ok := commands[command]; ok {
+					if err := cmd.Handler(); err != nil {
+						fmt.Println("Error:", err)
+					}
+				} else {
+					fmt.Print("Unknown command\n")
+				}
 			}
 			fmt.Print("Pokedex > ")
 		}
@@ -29,4 +52,26 @@ func cleanInput(text string) []string {
 		}
 	}
 	return words
+}
+
+type Command struct {
+	Name        string
+	Description string
+	Handler     func() error
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.Name, cmd.Description)
+	}
+	return nil
 }
